5c81fb5850703baf9546c451c65f3863eae7f04d
Lucasfi <lukafigueroa64@gmail.com>
2025-06-25 16:29:46 -0300

    corregimos un bug en el componente listaTareas

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,5 +1,5 @@
 import ListGroup from "react-bootstrap/ListGroup";
-import Itemtarea from "./Itemtarea";
+import Itemtarea from "./ItemTarea";
 
 const ListaTareas = ({tareas, borrarTarea}) => {
   return (
