879ba241e040e0df568d841a1e66d60d03bc581d
Lucasfi <lukafigueroa64@gmail.com>
2025-06-25 16:14:58 -0300

    agregamos estilo al texto de la tarea agregada

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -2,7 +2,7 @@ import { Button, ListGroup } from "react-bootstrap";
 
 const Itemtarea = ({nombreTarea, borrarTarea}) => {
   return (
-    <ListGroup.Item className="d-flex justify-content-between mb-2">{nombreTarea}<Button variant="danger" onClick={()=> borrarTarea(nombreTarea)}>Borrar</Button></ListGroup.Item>
+    <ListGroup.Item className="d-flex justify-content-between mb-2"><p className="mb-0 d-flex justify-content-center align-items-center"><strong>{nombreTarea}</strong></p><Button variant="danger" onClick={()=> borrarTarea(nombreTarea)}>Borrar</Button></ListGroup.Item>
   );
 };
 
